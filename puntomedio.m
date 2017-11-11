df900f1b7873fdcaa76296367cb8fdea9119f730
j02iT <32583456+j02iT@users.noreply.github.com>
2017-11-12 00:20:55 +0100

    Delete puntomedio.m

--- puntomedio.m
+++
@@ -1,9 +0,0 @@
-function I = puntomedio(f,a,b,N)
-%Sintaxis: I = puntomedio('f',a,b,N)
-%f: nombre del archivo .m donde se define la funcion a integrar
-%a,b: intervalo de integracion
-%N: numero de subintervalos
-h = (b-a)/N;
-x = a+h/2:h:b-h/2;
-y = feval(f,x);
-I = h*sum(y);
